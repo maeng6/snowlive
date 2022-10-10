48d6c16fed1bfb7c63e8643a5585d9541e571485
maeng6 <smalll0302@gmail.com>
2022-10-10 18:23:00 +0900

    maeng_0.0.8

---
+++ lib/model/m_shopModel.dart
@@ -0,0 +1,102 @@
+class ShopModel{
+  ShopModel({this.shopName,this.url,this.imageAsset});
+
+  String? shopName;
+  String? url;
+  String? imageAsset;
+}
+
+List<ShopModel> shopList = [
+  ShopModel(
+      shopName: '쇼군',
+      url: 'http://www.shogunshop.com/',
+      imageAsset: 'assets/imgs/logos/shop/shogun_logo.png'
+  ),
+  ShopModel(
+    shopName: '원에잇',
+    url: 'https://one8.co.kr/',
+      imageAsset: 'assets/imgs/logos/shop/one8_logo.png'
+  ),
+  ShopModel(
+      shopName: '911sports',
+      url: 'http://www.911sports.co.kr/',
+      imageAsset: 'assets/imgs/logos/shop/911sports_logo.png'
+  ),
+  ShopModel(
+      shopName: '팝스노우',
+      url: 'http://www.popsnowboard.co.kr/',
+      imageAsset: 'assets/imgs/logos/shop/popsnow_logo.png'
+  ),
+  ShopModel(
+      shopName: '베스트스노우',
+      url: 'http://www.bestsnowboard.co.kr/',
+      imageAsset: 'assets/imgs/logos/shop/bestsnow_logo.png'
+  ),
+  ShopModel(
+      shopName: '풍류',
+      url: 'http://www.poong-ryu.com/',
+      imageAsset: 'assets/imgs/logos/shop/pungryu_logo.png'
+  ),
+  ShopModel(
+      shopName: '보드코리아',
+      url: 'https://m.boardkorea.com/',
+      imageAsset: 'assets/imgs/logos/shop/boardkorea_logo.png'
+  ),
+  ShopModel(
+      shopName: '자이온',
+      url: 'http://www.zionsnowboard.com/',
+      imageAsset: 'assets/imgs/logos/shop/zion_logo.png'
+  ),
+  ShopModel(
+      shopName: '보드라인',
+      url: 'http://www.boardline.co.kr/',
+      imageAsset: 'assets/imgs/logos/shop/boardline_logo.png'
+  ),
+  ShopModel(
+      shopName: '강남스포츠',
+      url: 'http://www.gangnamsports.co.kr/',
+      imageAsset: 'assets/imgs/logos/shop/gangnam_logo.png'
+  ),
+
+];
+
+List<String?> shopNameList = [
+  shopList[0].shopName,
+  shopList[1].shopName,
+  shopList[2].shopName,
+  shopList[3].shopName,
+  shopList[4].shopName,
+  shopList[5].shopName,
+  shopList[6].shopName,
+  shopList[7].shopName,
+  shopList[8].shopName,
+  shopList[9].shopName,
+];
+
+
+List<String?> shopHomeUrlList = [
+  shopList[0].url,
+  shopList[1].url,
+  shopList[2].url,
+  shopList[3].url,
+  shopList[4].url,
+  shopList[5].url,
+  shopList[6].url,
+  shopList[7].url,
+  shopList[8].url,
+  shopList[9].url,
+];
+List<String?> shopImageAssetList = [
+  shopList[0].imageAsset,
+  shopList[1].imageAsset,
+  shopList[2].imageAsset,
+  shopList[3].imageAsset,
+  shopList[4].imageAsset,
+  shopList[5].imageAsset,
+  shopList[6].imageAsset,
+  shopList[7].imageAsset,
+  shopList[8].imageAsset,
+  shopList[9].imageAsset,
+];
+
+
--- lib/screens/brand/v_brandHome.dart
+++ lib/screens/brand/v_brandHome.dart
@@ -5,6 +5,7 @@ import 'package:get/get_core/src/get_main.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:snowlive3/screens/v_webPage.dart';
 import '../../model/m_brandModel.dart';
+import '../../model/m_shopModel.dart';
 
 class BrandWebBody extends StatefulWidget {
   BrandWebBody({Key? key}) : super(key: key);
@@ -17,6 +18,13 @@ class _BrandWebBodyState extends State<BrandWebBody> {
 
   List<String> labels = ['의류', '장비'];
   int counter = 0;
+  bool isBrand = true;
+
+  @override
+  void setState(VoidCallback fn) {
+    // TODO: implement setState
+    super.setState(fn);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -40,16 +48,25 @@ class _BrandWebBodyState extends State<BrandWebBody> {
                           child: Text(
                             '의류',
                             style: TextStyle(
-                                color: Color(0xFFFFFFFF), fontWeight: FontWeight.bold, fontSize: 15),
+                                color: (isBrand)
+                                    ?Color(0xFFFFFFFF)
+                                    :Color(0xFF111111),
+                                fontWeight: FontWeight.bold, fontSize: 15),
                           ),
-                          onPressed: () {},
+                          onPressed: () {
+                            isBrand= true;
+                            print('브랜드페이지로 전환');
+                            setState(() {});
+                          },
                           style: ElevatedButton.styleFrom(
                             minimumSize: Size(54, 32),
+                            backgroundColor: (isBrand)
+                                ?Color(0xFF111111)
+                                :Color(0xFFFFFFFF),
                             shape: RoundedRectangleBorder(
                                 borderRadius:
                                 BorderRadius.circular(8)),
                             elevation: 0,
-                            primary: Color(0xFF111111),
                           ),
                         ),
                         SizedBox(
@@ -57,18 +74,26 @@ class _BrandWebBodyState extends State<BrandWebBody> {
                         ),
                         ElevatedButton(
                           child: Text(
-                            '장비',
+                            '샵',
                             style: TextStyle(
-                                color: Color(0xFF111111)),
+                                color: (isBrand)
+                                    ?Color(0xFF111111)
+                                    :Color(0xFFFFFFFF)),
                           ),
-                          onPressed: () {},
+                          onPressed: () {
+                            isBrand= false;
+                            print('샵페이지로 전환');
+                            setState(() {});
+                          },
                           style: ElevatedButton.styleFrom(
                               minimumSize: Size(54, 32),
+                              backgroundColor: (isBrand)
+                                  ?Color(0xFFFFFFFF)
+                                  :Color(0xFF111111),
                               shape: RoundedRectangleBorder(
                                   borderRadius:
                                   BorderRadius.circular(6)),
                               elevation: 0,
-                              primary: Color(0xFFFFFFFF),
                               textStyle: TextStyle(
                                 fontSize: 15,
                                 fontWeight: FontWeight.bold,
@@ -95,7 +120,9 @@ class _BrandWebBodyState extends State<BrandWebBody> {
             ],
           ),
         ),
-        body:  clothWebGridView(context),
+        body:   (isBrand)
+            ?clothWebGridView(context)
+            :shopWebGridView(context),
       ),
     );
   }
@@ -158,6 +185,55 @@ Widget clothWebGridView(BuildContext context) {
                   ),
                 ),
               ],
+            ),
+          ),
+        ],
+      );
+    }),
+  );
+}
+Widget shopWebGridView(BuildContext context) {
+  final Size _size = MediaQuery.of(context).size;
+  return Padding(
+    padding: const EdgeInsets.only(left: 16, right: 16),
+    child: GridView.builder(
+    padding: EdgeInsets.only(top: 68, bottom: 16),
+    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 3,
+        mainAxisSpacing: 16,
+        crossAxisSpacing: 12,
+        childAspectRatio: 0.68),
+    itemCount: shopNameList.length,
+    itemBuilder: (BuildContext context, int index) {
+      return Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Expanded(
+            child: GestureDetector(
+              onTap: () =>
+                  Get.to(()=>WebPage(
+                      url: '${shopHomeUrlList[index]}')),
+              child: Container(
+                padding: EdgeInsets.all(10),
+                  width: _size.width * 0.3,
+                  decoration: BoxDecoration(
+                    border: Border.all(width: 1,color: Colors.grey),
+                    shape: BoxShape.circle,
+                      color: Colors.transparent,),
+                  child: ExtendedImage.asset(
+                    '${shopImageAssetList[index]}',
+                  )),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 5),
+            child: Text(
+              '${shopNameList[index]}',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 15,
+              ),
             ),
           ),
         ],
