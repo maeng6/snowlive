78188a03a8ecf8437ffd0a02ced584e9c57116de
minyundong <yj20130222@gmail.com>
2022-10-23 19:12:48 +0900

    min_0.0.9

--- lib/screens/comments/v_commentTile_resortHome.dart
+++ lib/screens/comments/v_commentTile_resortHome.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:extended_image/extended_image.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -43,6 +44,7 @@ class _CommentTile_resortHomeState extends State<CommentTile_resortHome> {
           .doc('resort')
           .collection('${_userModelController.instantResort.toString()}')
           .orderBy('timeStamp', descending: true)
+          .limit(4)
           .snapshots(),
       builder: (context,
           AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
@@ -58,86 +60,97 @@ class _CommentTile_resortHomeState extends State<CommentTile_resortHome> {
         }
         final chatDocs = snapshot.data!.docs;
 
-        return ListView.builder(
-          physics: NeverScrollableScrollPhysics(),
-          reverse: false,
-          itemCount: chatDocs.length,
-          itemBuilder: (context, index) {
-            String _time = _commentModelController.getAgoTime(chatDocs[index].get('timeStamp'));
-            return Container(
-              color: Colors.white,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SizedBox(
-                    height: 8,
-                  ),
-                  Row(
+        return Column(
+          children: [
+            CarouselSlider.builder(
+             options: CarouselOptions(
+               height: 200,
+               viewportFraction: 1,
+               reverse: false,
+               enableInfiniteScroll: false,
+               autoPlay: true,
+               autoPlayInterval: Duration(seconds: 3)
+             ),
+              itemCount: chatDocs.length,
+              itemBuilder: (context, index, pageViewIndex) {
+                String _time = _commentModelController.getAgoTime(chatDocs[index].get('timeStamp'));
+                return Container(
+                  width: MediaQuery.of(context).size.width,
+                  color: Colors.white,
+                  child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      if(chatDocs[index]['profileImageUrl'] != "")
-                       ExtendedImage.network(chatDocs[index]['profileImageUrl'],
-                         cache: true,
-                       shape: BoxShape.circle,
-                         borderRadius: BorderRadius.circular(20),
-                         width: 32,
-                         height: 32,
-                         fit: BoxFit.cover,
-                       ),
-                      if(chatDocs[index]['profileImageUrl'] == "")
-                        ExtendedImage.asset('assets/imgs/profile/img_profile_default_circle.png',
-                          shape: BoxShape.circle,
-                          borderRadius: BorderRadius.circular(20),
-                          width: 32,
-                          height: 32,
-                          fit: BoxFit.cover,
-                        ),
-                      SizedBox(width: 10),
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
+                      SizedBox(
+                        height: 8,
+                      ),
+                      Row(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Row(
+                          if(chatDocs[index]['profileImageUrl'] != "")
+                           ExtendedImage.network(chatDocs[index]['profileImageUrl'],
+                             cache: true,
+                           shape: BoxShape.circle,
+                             borderRadius: BorderRadius.circular(20),
+                             width: 32,
+                             height: 32,
+                             fit: BoxFit.cover,
+                           ),
+                          if(chatDocs[index]['profileImageUrl'] == "")
+                            ExtendedImage.asset('assets/imgs/profile/img_profile_default_circle.png',
+                              shape: BoxShape.circle,
+                              borderRadius: BorderRadius.circular(20),
+                              width: 32,
+                              height: 32,
+                              fit: BoxFit.cover,
+                            ),
+                          SizedBox(width: 10),
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
-                              Text(
-                                chatDocs[index].get('displayName'),
-                                style: TextStyle(fontWeight: FontWeight.bold),
+                              Row(
+                                children: [
+                                  Text(
+                                    chatDocs[index].get('displayName'),
+                                    style: TextStyle(fontWeight: FontWeight.bold),
+                                  ),
+                                  SizedBox(width: 5),
+                                  Text(
+                                    '$_time',
+                                    style: TextStyle(fontSize: 10),
+                                  ),
+                                ],
                               ),
-                              SizedBox(width: 5),
-                              Text(
-                                '$_time',
-                                style: TextStyle(fontSize: 10),
-                              ),
+                              Row(
+                                children: [
+                                  Container(
+                                    constraints: BoxConstraints(
+                                        maxWidth: 250
+                                    ),
+                                    child: Text(chatDocs[index].get('comment'),
+                                      maxLines:2,
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                  ),
+                                ],
+                              )
                             ],
                           ),
-                          Row(
-                            children: [
-                              Container(
-                                constraints: BoxConstraints(
-                                    maxWidth: 250
-                                ),
-                                child: Text(chatDocs[index].get('comment'),
-                                  maxLines:2,
-                                  overflow: TextOverflow.ellipsis,
-                                ),
-                              ),
-                            ],
-                          )
                         ],
+                      ),
+                      SizedBox(
+                        height: 8,
+                      ),
+                      Divider(
+                        color: Color(0xFFF5F5F5),
+                        thickness: 1,
                       ),
                     ],
                   ),
-                  SizedBox(
-                    height: 8,
-                  ),
-                  Divider(
-                    color: Color(0xFFF5F5F5),
-                    thickness: 1,
-                  ),
-                ],
-              ),
-            );
-          },
+                );
+              },
+            ),
+          ],
         );
       },
     );
