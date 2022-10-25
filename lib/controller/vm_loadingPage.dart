c234de81df4b81291eed8aae1f8b4279dc2a60f2
maeng6 <smalll0302@gmail.com>
2022-10-25 22:47:33 +0900

    maeng_0.2.1

--- lib/controller/vm_loadingPage.dart
+++ lib/controller/vm_loadingPage.dart
@@ -1,9 +1,12 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:get/get.dart';
 import 'package:snowlive3/controller/vm_getDateTimeController.dart';
 import 'package:snowlive3/controller/vm_resortModelController.dart';
 import 'package:snowlive3/controller/vm_userModelController.dart';
+import 'package:snowlive3/screens/onboarding/v_WelcomePage.dart';
 import 'package:snowlive3/screens/v_MainHome.dart';
 import 'package:snowlive3/screens/v_loginpage.dart';
 
@@ -32,9 +35,23 @@ class _LoadingPageState extends State<LoadingPage> {
         print('login상태냐?? -> ${snapshot.hasData}');
         if (snapshot.connectionState == ConnectionState.done &&
             snapshot.hasData == true) {
+          print('1');
           return MainHome();
         } else {
-          return LoginPage();
+          if(FirebaseAuth.instance.currentUser != null) {
+            print(FirebaseAuth.instance.currentUser != null);
+            return FutureBuilder(
+                future:FirebaseFirestore.instance.collection('user').doc(FirebaseAuth.instance.currentUser!.uid).delete(),
+                builder: (context, AsyncSnapshot<dynamic> snapshot){
+                  return FutureBuilder(
+                      future: FirebaseAuth.instance.currentUser!.delete(),
+                      builder: (context, AsyncSnapshot<dynamic> snapshot){
+                        return LoginPage();
+                      }
+                  );
+                }
+            );
+          }else LoginPage();
         }
         return LoginPage();
       },
