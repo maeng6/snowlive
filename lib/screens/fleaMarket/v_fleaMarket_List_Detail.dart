9237ce638e64cb01300ba6105ea1ebf11dd80b8c
minyundong <yj20130222@gmail.com>
2023-01-28 23:42:53 +0900

    min_0.2.1

--- lib/screens/fleaMarket/v_fleaMarket_List_Detail.dart
+++ lib/screens/fleaMarket/v_fleaMarket_List_Detail.dart
@@ -8,6 +8,7 @@ import 'package:snowlive3/controller/vm_fleaMarketController.dart';
 import 'package:snowlive3/controller/vm_userModelController.dart';
 import 'package:snowlive3/screens/fleaMarket/v_fleaMarket_Chatroom.dart';
 import 'package:snowlive3/screens/fleaMarket/v_fleaMarket_ModifyPage.dart';
+import 'package:snowlive3/screens/v_MainHome.dart';
 import 'package:snowlive3/widget/w_fullScreenDialog.dart';
 
 class FleaMarket_List_Detail extends StatefulWidget {
@@ -54,6 +55,288 @@ class _FleaMarket_List_DetailState extends State<FleaMarket_List_Detail> {
                   Get.back();
                 },
               ),
+              actions: [
+                (_fleaModelController.uid != _userModelController.uid)
+                    ? GestureDetector(
+                  onTap: () =>
+                      showModalBottomSheet(
+                          enableDrag: false,
+                          context: context,
+                          builder:
+                              (context) {
+                            return Container(
+                              height: 190,
+                              child:
+                              Padding(
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 20.0,
+                                    vertical: 14),
+                                child:
+                                Column(
+                                  children: [
+                                    GestureDetector(
+                                      child: ListTile(
+                                        contentPadding: EdgeInsets.zero,
+                                        title: Center(
+                                          child: Text(
+                                            '신고하기',
+                                            style: TextStyle(
+                                              fontSize: 15,
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                          ),
+                                        ),
+                                        //selected: _isSelected[index]!,
+                                        onTap: () async {
+                                          Get.dialog(AlertDialog(
+                                            contentPadding: EdgeInsets.only(bottom: 0, left: 20, right: 20, top: 30),
+                                            elevation: 0,
+                                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+                                            buttonPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 0),
+                                            content: Text(
+                                              '이 회원을 신고하시겠습니까?',
+                                              style: TextStyle(fontWeight: FontWeight.w600, fontSize: 15),
+                                            ),
+                                            actions: [
+                                              Row(
+                                                children: [
+                                                  TextButton(
+                                                      onPressed: () {
+                                                        Navigator.pop(context);
+                                                      },
+                                                      child: Text(
+                                                        '취소',
+                                                        style: TextStyle(
+                                                          fontSize: 15,
+                                                          color: Color(0xFF949494),
+                                                          fontWeight: FontWeight.bold,
+                                                        ),
+                                                      )),
+                                                  TextButton(
+                                                      onPressed: () async {
+                                                        var repoUid = _fleaModelController.uid;
+                                                        await _userModelController.repoUpdate(repoUid);
+                                                        Navigator.pop(context);
+                                                        Navigator.pop(context);
+                                                      },
+                                                      child: Text(
+                                                        '신고',
+                                                        style: TextStyle(
+                                                          fontSize: 15,
+                                                          color: Color(0xFF3D83ED),
+                                                          fontWeight: FontWeight.bold,
+                                                        ),
+                                                      ))
+                                                ],
+                                                mainAxisAlignment: MainAxisAlignment.end,
+                                              )
+                                            ],
+                                          ));
+                                        },
+                                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                                      ),
+                                    ),
+                                    GestureDetector(
+                                      child: ListTile(
+                                        contentPadding: EdgeInsets.zero,
+                                        title: Center(
+                                          child: Text(
+                                            '이 회원의 글 모두 숨기기',
+                                            style: TextStyle(
+                                              fontSize: 15,
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                          ),
+                                        ),
+                                        //selected: _isSelected[index]!,
+                                        onTap: () async {
+                                          Get.dialog(AlertDialog(
+                                            contentPadding: EdgeInsets.only(bottom: 0, left: 20, right: 20, top: 30),
+                                            elevation: 0,
+                                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+                                            buttonPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 0),
+                                            content: Text(
+                                              '이 회원의 게시물을 모두 숨길까요?\n이 동작은 취소할 수 없습니다.',
+                                              style: TextStyle(fontWeight: FontWeight.w600, fontSize: 15),
+                                            ),
+                                            actions: [
+                                              Row(
+                                                children: [
+                                                  TextButton(
+                                                      onPressed: () {
+                                                        Navigator.pop(context);
+                                                      },
+                                                      child: Text(
+                                                        '취소',
+                                                        style: TextStyle(
+                                                          fontSize: 15,
+                                                          color: Color(0xFF949494),
+                                                          fontWeight: FontWeight.bold,
+                                                        ),
+                                                      )),
+                                                  TextButton(
+                                                      onPressed: () {
+                                                        var repoUid = _fleaModelController.uid;
+                                                        _userModelController.updateRepoUid(repoUid);
+                                                        Navigator.pop(context);
+                                                        Navigator.pop(context);
+                                                        Navigator.pop(context);
+                                                      },
+                                                      child: Text(
+                                                        '확인',
+                                                        style: TextStyle(
+                                                          fontSize: 15,
+                                                          color: Color(0xFF3D83ED),
+                                                          fontWeight: FontWeight.bold,
+                                                        ),
+                                                      ))
+                                                ],
+                                                mainAxisAlignment: MainAxisAlignment.end,
+                                              )
+                                            ],
+                                          ));
+                                        },
+                                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ),
+                            );
+                          }),
+                  child: Padding(
+                    padding:
+                    const EdgeInsets.only(bottom: 22),
+                    child: Icon(Icons.more_vert,
+                      color: Color(0xFFdedede),
+                    ),
+                  ),
+                )
+                    : GestureDetector(
+                  onTap: () =>
+                      showModalBottomSheet(
+                          enableDrag:
+                          false,
+                          context:
+                          context,
+                          builder:
+                              (context) {
+                            return Container(
+                              height:
+                              130,
+                              child:
+                              Padding(
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal:
+                                    20.0,
+                                    vertical:
+                                    14),
+                                child:
+                                Column(
+                                  children: [
+                                    GestureDetector(
+                                      child: ListTile(
+                                        contentPadding: EdgeInsets.zero,
+                                        title: Center(
+                                          child: Text(
+                                            '삭제',
+                                            style: TextStyle(
+                                              fontSize: 15,
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                          ),
+                                        ),
+                                        //selected: _isSelected[index]!,
+                                        onTap: () async {
+                                          Navigator.pop(context);
+                                          showModalBottomSheet(
+                                              context: context,
+                                              builder: (context) {
+                                                return Container(
+                                                  color: Colors.white,
+                                                  height: 180,
+                                                  child: Padding(
+                                                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                                                    child: Column(
+                                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                                      mainAxisAlignment: MainAxisAlignment.start,
+                                                      children: [
+                                                        SizedBox(
+                                                          height: 30,
+                                                        ),
+                                                        Text(
+                                                          '삭제하시겠습니까?',
+                                                          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Color(0xFF111111)),
+                                                        ),
+                                                        SizedBox(
+                                                          height: 30,
+                                                        ),
+                                                        Row(
+                                                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                                          children: [
+                                                            Expanded(
+                                                              child: ElevatedButton(
+                                                                onPressed: () {
+                                                                  Navigator.pop(context);
+                                                                },
+                                                                child: Text(
+                                                                  '취소',
+                                                                  style: TextStyle(color: Colors.white, fontSize: 15, fontWeight: FontWeight.bold),
+                                                                ),
+                                                                style: TextButton.styleFrom(splashFactory: InkRipple.splashFactory, elevation: 0, minimumSize: Size(100, 56), backgroundColor: Color(0xff555555), padding: EdgeInsets.symmetric(horizontal: 0)),
+                                                              ),
+                                                            ),
+                                                            SizedBox(
+                                                              width: 10,
+                                                            ),
+                                                            Expanded(
+                                                              child: ElevatedButton(
+                                                                onPressed: () async {
+                                                                  CustomFullScreenDialog.showDialog();
+                                                                  try {
+                                                                    await FirebaseFirestore.instance.collection('fleaMarket').doc('${_userModelController.uid}#${_fleaModelController.fleaCount}').delete();
+                                                                    Navigator.pop(context);
+                                                                  } catch (e) {}
+                                                                  print('댓글 삭제 완료');
+                                                                  Navigator.pop(context);
+                                                                  CustomFullScreenDialog.cancelDialog();
+                                                                },
+                                                                child: Text(
+                                                                  '확인',
+                                                                  style: TextStyle(color: Colors.white, fontSize: 15, fontWeight: FontWeight.bold),
+                                                                ),
+                                                                style: TextButton.styleFrom(splashFactory: InkRipple.splashFactory, elevation: 0, minimumSize: Size(100, 56), backgroundColor: Color(0xff2C97FB), padding: EdgeInsets.symmetric(horizontal: 0)),
+                                                              ),
+                                                            ),
+                                                          ],
+                                                        )
+                                                      ],
+                                                    ),
+                                                  ),
+                                                );
+                                              });
+                                        },
+                                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            );
+                          }),
+                  child: Padding(
+                    padding:
+                    const EdgeInsets
+                        .only(
+                        bottom: 22),
+                    child: Icon(
+                      Icons.more_vert,
+                      color: Color(
+                          0xFFdedede),
+                    ),
+                  ),
+                )
+              ],
               backgroundColor: Colors.white,
               elevation: 0.0,
             ),
