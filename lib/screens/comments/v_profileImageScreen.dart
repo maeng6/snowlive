e8e2c8959d5872407e2045729a205363becfe4a9
minyundong <yj20130222@gmail.com>
2023-02-21 23:43:53 +0900

    min_0.4.0

--- lib/screens/comments/v_profileImageScreen.dart
+++ lib/screens/comments/v_profileImageScreen.dart
@@ -1,6 +1,7 @@
 import 'package:extended_image/extended_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:snowlive3/screens/comments/v_liveTalk_Screen.dart';
 
 class ProfileImagePage extends StatefulWidget {
   ProfileImagePage({Key? key, required this.CommentProfileUrl}) : super(key: key);
@@ -14,36 +15,42 @@ class ProfileImagePage extends StatefulWidget {
 class _ProfileImagePageState extends State<ProfileImagePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(58),
-        child: AppBar(
-          systemOverlayStyle: SystemUiOverlayStyle.dark,
-          leading: GestureDetector(
-            child: Icon(Icons.close,
-            color: Colors.white,
-            ),
-            onTap: () {
-              Navigator.pop(context);
-            },
-          ),
-          elevation: 0.0,
-        ),
-      ),
-      body: InteractiveViewer(
-        maxScale: 7,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            if(widget.CommentProfileUrl != '')
-            ExtendedImage.network(widget.CommentProfileUrl),
-            if(widget.CommentProfileUrl == '')
-              ExtendedImage.asset(
-                'assets/imgs/profile/img_profile_default_.png',
+    return Dismissible(
+      background: CommentTile_liveTalk_resortHome(),
+      direction: DismissDirection.down,
+      key: UniqueKey(),
+      onDismissed: (_)=>Navigator.pop(context),
+      child: Scaffold(
+        backgroundColor: Colors.black,
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(58),
+          child: AppBar(
+            systemOverlayStyle: SystemUiOverlayStyle.dark,
+            leading: GestureDetector(
+              child: Icon(Icons.close,
+              color: Colors.white,
               ),
-          ],
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            elevation: 0.0,
+          ),
+        ),
+        body: InteractiveViewer(
+          maxScale: 7,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              if(widget.CommentProfileUrl != '')
+              ExtendedImage.network(widget.CommentProfileUrl),
+              if(widget.CommentProfileUrl == '')
+                ExtendedImage.asset(
+                  'assets/imgs/profile/img_profile_default_.png',
+                ),
+            ],
+          ),
         ),
       ),
     );
