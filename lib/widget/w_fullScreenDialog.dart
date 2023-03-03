398ab0d9b742442cc35891298cdebf39ea4fb794
8choon <hooneekimm@naver.com>
2023-03-04 02:21:33 +0900

    hoon_0.7.0

--- lib/widget/w_fullScreenDialog.dart
+++ lib/widget/w_fullScreenDialog.dart
@@ -1,21 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:lottie/lottie.dart';
+
 
 class CustomFullScreenDialog {
+
   static void showDialog() {
     Get.dialog(
       WillPopScope(
-        child: Container(
-          child: Center(
-            child: CircularProgressIndicator(
-              valueColor: AlwaysStoppedAnimation(Colors.blueAccent),
-            ),
-          ),
-        ),
+        child: Lottie.asset('assets/json/loadings_wht_final.json'),
         onWillPop: () => Future.value(false),
       ),
       barrierDismissible: false,
-      barrierColor: Color(0xff141A31).withOpacity(.3),
+      barrierColor: Color(0xff000000).withOpacity(.45),
       useSafeArea: true,
     );
   }
