cbb9711b1135794ce4fc9e03995e974af07ebe3d
8choon <hooneekimm@naver.com>
2023-07-06 19:55:19 +0900

    hoon_0.9.3

--- lib/model/m_slopeLocationModel.dart
+++ lib/model/m_slopeLocationModel.dart
@@ -10,11 +10,11 @@ class LocationModel {
 List<LocationModel> locations = [
   LocationModel(
     name: '휘닉스평창 1',
-    coordinates: LatLng(37.391105, 127.109500),
+    coordinates: LatLng(37.390613, 127.108915),
   ),
   LocationModel(
     name: '휘닉스평창 2',
-    coordinates: LatLng(37.578337, 128.310873),
+    coordinates: LatLng(37.390640, 127.108990),
   ),
   // 추가적인 위치들을 여기에 추가
 ];
