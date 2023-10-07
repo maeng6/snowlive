fc947e246b6c8a94d804f02a53b90e072062feff
minyundong <yj20130222@gmail.com>
2023-10-07 17:12:39 +0900

    min_0.1.7

--- lib/controller/vm_loadingPage.dart
+++ lib/controller/vm_loadingPage.dart
@@ -1,19 +1,7 @@
-import 'package:com.snowlive/controller/vm_urlLauncherController.dart';
 import 'package:com.snowlive/controller/vm_versionController.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:com.snowlive/controller/vm_friendsCommentController.dart';
-import 'package:com.snowlive/controller/vm_getDateTimeController.dart';
-import 'package:com.snowlive/controller/vm_liveCrewModelController.dart';
-import 'package:com.snowlive/controller/vm_liveMapController.dart';
 import 'package:com.snowlive/controller/vm_loginController.dart';
-import 'package:com.snowlive/controller/vm_mainHomeController.dart';
-import 'package:com.snowlive/controller/vm_noticeController.dart';
-import 'package:com.snowlive/controller/vm_rankingTierModelController.dart';
-import 'package:com.snowlive/controller/vm_resortModelController.dart';
-import 'package:com.snowlive/controller/vm_seasonController.dart';
-import 'package:com.snowlive/controller/vm_timeStampController.dart';
-import 'package:com.snowlive/controller/vm_userModelController.dart';
 import 'package:com.snowlive/screens/v_MainHome.dart';
 import 'package:com.snowlive/screens/login/v_loginpage.dart';
 
@@ -28,17 +16,6 @@ class _LoadingPageState extends State<LoadingPage> {
   @override
   void initState() {
     super.initState();
-
-    // Dependency Injection
-    Get.put(ResortModelController(), permanent: true);
-    Get.put(UserModelController(), permanent: true);
-    Get.put(GetDateTimeController(), permanent: true);
-    Get.put(TimeStampController(), permanent: true);
-    Get.put(FriendsCommentModelController(), permanent: true);
-    Get.put(MainHomeController(), permanent: true);
-    Get.put(LiveCrewModelController(), permanent: true);
-    Get.put(SeasonController(), permanent: true);
-    Get.put(UrlLauncherController(), permanent: true);
 
     checkForUpdate();
 
--- lib/screens/v_splashScreen.dart
+++ lib/screens/v_splashScreen.dart
@@ -1,14 +1,35 @@
 import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:com.snowlive/controller/vm_friendsCommentController.dart';
+import 'package:com.snowlive/controller/vm_getDateTimeController.dart';
+import 'package:com.snowlive/controller/vm_liveCrewModelController.dart';
+import 'package:com.snowlive/controller/vm_mainHomeController.dart';
+import 'package:com.snowlive/controller/vm_resortModelController.dart';
+import 'package:com.snowlive/controller/vm_seasonController.dart';
+import 'package:com.snowlive/controller/vm_timeStampController.dart';
+import 'package:com.snowlive/controller/vm_urlLauncherController.dart';
+import 'package:com.snowlive/controller/vm_userModelController.dart';
 import 'package:extended_image/extended_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:com.snowlive/controller/vm_loadingPage.dart';
+import 'package:get/get.dart';
 class SplashScreen extends StatelessWidget {
   final String imageUrl;
   SplashScreen({Key? key, required this.imageUrl}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+
+    // Dependency Injection
+    Get.put(ResortModelController(), permanent: true);
+    Get.put(UserModelController(), permanent: true);
+    Get.put(GetDateTimeController(), permanent: true);
+    Get.put(TimeStampController(), permanent: true);
+    Get.put(FriendsCommentModelController(), permanent: true);
+    Get.put(MainHomeController(), permanent: true);
+    Get.put(LiveCrewModelController(), permanent: true);
+    Get.put(SeasonController(), permanent: true);
+    Get.put(UrlLauncherController(), permanent: true);
 
     return AnimatedSplashScreen(
         curve: Curves.easeInCirc,
