84d7d7eac5ecdd70db3265c78a95944950c59684
8choon <hooneekimm@naver.com>
2023-12-05 22:56:22 +0900

    hoon_2.4.7

--- lib/screens/Ranking/v_Ranking_indi_All_Screen.dart
+++ lib/screens/Ranking/v_Ranking_indi_All_Screen.dart
@@ -137,6 +137,7 @@ class _RankingIndiAllScreenState extends State<RankingIndiAllScreen> {
         child: Padding(
           padding: const EdgeInsets.only(left: 16, right: 16),
           child: SingleChildScrollView(
+            physics: AlwaysScrollableScrollPhysics(),
             child: Column(
               children: documents!.map((document) {
                 final itemKey = document['uid'] == _userModelController.uid
