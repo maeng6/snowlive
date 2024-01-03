b8881b85248d9e04aef5c743c0dd653067d79e93
8choon <hooneekimm@naver.com>
2024-01-03 22:52:47 +0900

    hoon_2.6.8

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/maeng_6/Developer/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/maeng_6/project/snowlive"
+export "FLUTTER_ROOT=/Users/8choon/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/8choon/project/snowlive"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=3.3.6"
-export "FLUTTER_BUILD_NUMBER=66"
+export "FLUTTER_BUILD_NAME=3.4.0"
+export "FLUTTER_BUILD_NUMBER=70"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
