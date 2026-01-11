ed80e146b744ca800d0f382a71b0461e0e30df33
yundong min <yundongmin@yundongui-MacBookPro.local>
2026-01-11 15:44:46 +0900

    min_1.6.6

--- lib/view/fleamarket/v_fleaMarketDetail.dart
+++ lib/view/fleamarket/v_fleaMarketDetail.dart
@@ -1284,8 +1284,9 @@ class _FleaMarketDetailViewState extends State<FleaMarketDetailView> {
                                                   ),
                                                 ),
                                               ),
-                                              // 끌어올리기 버튼 (거래완료가 아닐 때만 표시)
-                                              if (_fleamarketDetailViewModel.fleamarketDetail.status != FleamarketStatus.soldOut.korean)
+                                              // 끌어올리기 버튼 (내 게시물이고 거래완료가 아닐 때만 표시)
+                                              if (_fleamarketDetailViewModel.fleamarketDetail.userId == _userViewModel.user.user_id &&
+                                                  _fleamarketDetailViewModel.fleamarketDetail.status != FleamarketStatus.soldOut.korean)
                                                 Padding(
                                                   padding: EdgeInsets.only(top: 10),
                                                   child: GestureDetector(
